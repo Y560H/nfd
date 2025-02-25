526393cf98c91a6f4ed7a6f01c00f2a408babc7b
Y560H <y560h@outlook.com>
2025-02-25 09:55:20 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -4,9 +4,9 @@ const SECRET = ENV_BOT_SECRET // A-Z, a-z, 0-9, _ and -
 const ADMIN_UID = ENV_ADMIN_UID // your user id, get it from https://t.me/username_to_id_bot
 
 const NOTIFY_INTERVAL = 3600 * 1000;
-const fraudDb = 'https://raw.githubusercontent.com/LloydAsp/nfd/main/data/fraud.db';
-const notificationUrl = 'https://raw.githubusercontent.com/LloydAsp/nfd/main/data/notification.txt'
-const startMsgUrl = 'https://raw.githubusercontent.com/LloydAsp/nfd/main/data/startMessage.md';
+const fraudDb = 'https://raw.githubusercontent.com/Y560H/nfd/main/data/fraud.db';
+const notificationUrl = 'https://raw.githubusercontent.com/Y560H/nfd/main/data/notification.txt'
+const startMsgUrl = 'https://raw.githubusercontent.com/Y560H/nfd/main/data/startMessage.md';
 
 const enable_notification = true
 /**
@@ -107,16 +107,25 @@ async function onMessage (message) {
     if(!message?.reply_to_message?.chat){
       return sendMessage({
         chat_id:ADMIN_UID,
-        text:'使用方法，回复转发的消息，并发送回复消息，或者`/block`、`/unblock`、`/checkblock`等指令'
+        text:'使用方法，回复转发的消息，并发送回复消息，或者`/block`、`/unblock`、`/checkblock`、`/封禁`、`/解封`、`/查`等指令'
       })
     }
     if(/^\/block$/.exec(message.text)){
       return handleBlock(message)
     }
+    if(/^\/[\u5C01\u7981]$/.exec(message.text)){
+      return handleBlock(message)
+    }
     if(/^\/unblock$/.exec(message.text)){
       return handleUnBlock(message)
     }
+    if(/^\/[\u89E3\u5C01]$/.exec(message.text)){
+      return handleUnBlock(message)
+    }
     if(/^\/checkblock$/.exec(message.text)){
+      return checkBlock(message)
+    }
+    if(/^\/[\67E5]$/.exec(message.text)){
       return checkBlock(message)
     }
     let guestChantId = await nfd.get('msg-map-' + message?.reply_to_message.message_id,
@@ -137,7 +146,7 @@ async function handleGuestMessage(message){
   if(isblocked){
     return sendMessage({
       chat_id: chatId,
-      text:'Your are blocked'
+      text:'Your are blocked \n 你已被封禁'
     })
   }
 
